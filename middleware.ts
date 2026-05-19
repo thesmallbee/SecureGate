721141ae5e1dcb94db661ffeb8482531e3ce5cc8
Fosu Kwasi Daniel <fosukwasidaniel54@gmail.com>
2026-05-19 15:44:08 +0000

    refactor: simplify middleware by using the default next-auth middleware export

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,4 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function middleware(req: NextRequest) {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-    secureCookie: req.url?.startsWith("https://"),
-  });
-
-  if (!token) {
-    const url = new URL("/login", req.url);
-    url.searchParams.set("callbackUrl", req.nextUrl.pathname);
-    return NextResponse.redirect(url);
-  }
-
-  return NextResponse.next();
-}
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/dashboard/:path*"],
